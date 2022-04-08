9bc162dfb1aca9746d0b5cb5bdfb29b8e0ca15b6
Andrew <solod.andrew93@gmail.com>
2022-04-08 22:57:39 +0300

    test

--- src/com/gymfox/lab.java
+++ src/com/gymfox/lab.java
@@ -4,17 +4,12 @@ import java.time.LocalDate;
 
 public class lab {
     public static void main(String[] args) {
-//        LocalDate myBirthday = LocalDate.of(1993, 12, 13);
-//
-//        System.out.println(myBirthday.getDayOfWeek() + ", " + myBirthday.getDayOfYear() + ", " + myBirthday.getMonth() + ", " + myBirthday.getYear());
-//
-//        LocalDate dateNow = LocalDate.now();
-//
-//        System.out.println(String.format("Date is now: %s.\nCurrent date was after my birthday? - %b\nCurrent date was before my birthday? - %b", dateNow, dateNow.isAfter(myBirthday), dateNow.isBefore(myBirthday)));
+        LocalDate myBirthday = LocalDate.of(1993, 12, 13);
 
-        float[] f1,f2;
-        f1 = new float[10];
-        f2 = f1;
-        System.out.println("f2[0] = " + f2[0]);
+        System.out.println(myBirthday.getDayOfWeek() + ", " + myBirthday.getDayOfYear() + ", " + myBirthday.getMonth() + ", " + myBirthday.getYear());
+
+        LocalDate dateNow = LocalDate.now();
+
+        System.out.println(String.format("Date is now: %s.\nCurrent date was after my birthday? - %b\nCurrent date was before my birthday? - %b", dateNow, dateNow.isAfter(myBirthday), dateNow.isBefore(myBirthday)));
     }
 }
