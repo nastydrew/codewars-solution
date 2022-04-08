25b4eb9f6611dfaddaa73ff3c30ea0faefe0d7d2
Andrew <solod.andrew93@gmail.com>
2022-04-08 22:04:59 +0300

    Removed redundant code

--- src/com/gymfox/lab.java
+++ src/com/gymfox/lab.java
@@ -18,27 +18,3 @@ public class lab {
         System.out.println("f2[0] = " + f2[0]);
     }
 }
-
-class Mixer {
-    Mixer m1;
-
-    Mixer() {}
-
-    Mixer(Mixer mixer) {
-        m1 = mixer;
-    }
-
-    void go() {
-        System.out.print("hi ");
-    }
-
-    public static void main(String[] args) {
-        Mixer m2 = new Mixer();
-        Mixer m3 = new Mixer(m2);
-        m3.go();
-        Mixer m4 = m3.m1;
-        m4.go();
-        Mixer m5 = m2.m1;
-        m5.go();
-    }
-}
